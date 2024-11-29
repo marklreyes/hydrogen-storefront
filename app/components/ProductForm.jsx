dfaeee08effa80a01f5ca8443bceb8cf57c027f2
Mark Reyes <mr@marklreyes.com>
2024-11-29 11:45:05 -0800

    Add Tailwind v4 Alpha classes to Add to Cart button

--- app/components/ProductForm.jsx
+++ app/components/ProductForm.jsx
@@ -43,6 +43,7 @@ export function ProductForm({product, selectedVariant, variants}) {
       >
         {selectedVariant?.availableForSale ? 'Add to cart' : 'Sold out'}
       </AddToCartButton>
+			&nbsp;
       <ShopPayButton
         variantIds={[selectedVariant.id]}
         storeDomain={'https://demostore.mock.shop/'}
